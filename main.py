a300208e9cf96f9db395d1c06c175665421caa8d
Clint <cdunn6754@yahoo.com/>
2017-01-12 18:55:32 -0700

    Finished back prop, cost fcn, other misc, gettin close

--- main.py
+++ main.py
@@ -6,26 +6,53 @@ from nn_functions import sigmoid
 #..................................................#
 # Decide on Neural Network Parameters
 hl = [20,10] # hidden layers (eg. [10,10] means 2 layers, 10 units each)
+lam = 1e-5
+number_output_class = 10
 
 # Import our MNIST test data
 train_df = pd.read_csv('Data/train.csv')
+m = float(train_df.shape[0]) # number of training samples
+
 
 # Transfer to numpy arrays
 X_train = np.array(train_df.drop(['label'], axis=1))
 Y_train = np.array(train_df['label'])
+Y_train = nf.one_hot(Y_train,number_output_class)
 
-# NN weights matrix (a list of 2-d arrays
-l = np.append( [np.ma.shape(X_train)[1]], np.append(hl, [10])) 
+# l is a vector which describes the number of units in all layers of the nn
+l = np.append( [np.ma.shape(X_train)[1]], np.append(hl, [number_output_class])) 
 # l = [ # units input (784 for our pictures), hidden layer sizes, # output units]
 
 # Weights matrices (Theta is a list of 2d matrices)
+# the matrices have shape [# units next layer, # units previous layer]
 Theta = []    
 for i in range(len(l) - 1):
     theta_2d = np.random.rand(l[i+1], l[i] + 1) /100
     Theta.append(theta_2d)
 
-y_train = [0 ,1, 0, 0, 0, 0, 0, 0, 0, 0]
-Delta =nf.back_prop(Theta, X_train[1,:], y_train)
+#Delta =nf.back_prop(Theta, X_train[1,:], y_train)
 
-for i in Delta:
-    print(np.shape(i))
+J = nf.cost_function(Theta,X_train,Y_train, lam)
+#A = nf.forward_prop(Theta,X_train[0,:])
+
+# Loop for backprop, turned into a mess
+# Have to do the first one out of loop to get the list going
+# then do nested loops to add matrices from other training examples to 
+# the running sum
+Delta = nf.back_prop(Theta,X_train[0,:],Y_train[:,0])
+trials = np.arange(1,int(m)-1)
+for i in trials:
+    temp_Delta = nf.back_prop(Theta,X_train[i,:], Y_train[:,i])
+    for j in range(len(Delta)):
+        mat = Delta[j]
+        mat = mat + temp_Delta[j]
+
+# Make a Theta without the bias weights
+reg_Theta = Theta
+for i in range(len(reg_Theta)):
+    reg_Theta[i][:,0] = 0
+
+print reg_Theta[2][:,0]
+
+    
+
--- nn_functions.py
+++ nn_functions.py
@@ -1,16 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+# sigmoid for forward prop
 def sigmoid(z):
     return 1/(1 + np.e ** -z)
 
+# unroll the solution vector into matrix of one-hot vectors
+# returns them in matrix shape [outputclasses, number of training examples]
+def one_hot(vec, num_classes):
+    t_examples = len(vec)  #training examples
+    one_hot_mat = np.zeros([t_examples, num_classes]) 
+    column_start_index = np.arange(t_examples) * num_classes
+    one_hot_mat.flat[column_start_index + vec.ravel()] = 1
+    return np.transpose(one_hot_mat)
 
+# Forward prop for cost function that can run n training examples simultaneously
+# and returns only the output of the nn, not every activation
+def h_theta(Theta, X_train):
+    h = np.hstack((np.ones((X_train.shape[0],1)), X_train)) #add bias
+    h = np.transpose(h)
+    for i in range(len(Theta)):
+        print ("Iteration %s in the h_theta loop" %i)
+        h = np.matmul(Theta[i],h)
+        h = sigmoid(h)
+        if i < (len(Theta) -1): # add bias but not to the final output
+            h = np.vstack((np.ones((1,h.shape[1])),h))
+    return h
+
+
+# forward prop for use with backprop (one training example at a time)
 def forward_prop(Theta, x_train):
-    # Similar to Theta make list A which will hold the activations  through
-    # the network and will be returned to be used with back_prop
+    # Similar to Theta make list A which will hold the activation vectors
+    # through the network and will be returned to be used with back_prop
     x_train = np.append([1], x_train) # add in the bias unit
     A = [x_train]
+    # loop through layers but do so for every training example at once
     for i in range(len(Theta)):
         theta = Theta[i]
         a = sigmoid(np.matmul(theta,A[i]))
@@ -24,21 +48,36 @@ def back_prop(Theta, x_train, y_train):
     A = forward_prop(Theta, x_train)
     last_delta = A[-1] - y_train # delta of the output layer
     Delta = list() # empty list to hold delta (cap.delta)
-    for i in range(len(Theta)):
-        print('Iteration of the bp loop %s\n'  %(str(i)))
-        previous_a = A[-1 - (i + 1)]# lose the bias
-        if not i == 0: #except for output layer, lose the bias
+    for k in range(len(Theta)):
+        previous_a = A[-1 - (k + 1)]# lose the bias
+        if not k == 0: #except for output layer, lose the bias
             last_delta = last_delta[1:] 
-        previous_delta = np.matmul(np.transpose(Theta[-1 - i]), last_delta) \
+        previous_delta = np.matmul(np.transpose(Theta[-1 - k]), last_delta) \
                 * previous_a * (1 - previous_a)
         Delta.insert(0,np.outer(last_delta, previous_a))
         last_delta = previous_delta
 
     return Delta
 
-def cost_function(Theta, X_list, 
+def cost_function(Theta, X_train, Y_train, lam):
+    m = float(X_train.shape[0]) # # of training examples
+    h = h_theta(Theta, X_train) # returns matrix of h(theta) in shape 
+                                # [output features, training examples]
+    
+    # Regularlization
+    reg = 0
+    for theta in Theta:
+        reg = sum(sum(theta[:,1:]**2)) + reg
 
-        
-        
+    # Compute the Cost function J
+    J = (1/m) * sum(sum(-Y_train * np.log(h) - (1 - Y_train) * np.log(1 - h))) \
+        + (lam/(2*m)) * reg
+    
+    return J
 
+
+    
+    
+
+    
         
